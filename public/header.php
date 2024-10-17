1ab9e81e934c529c7eef6880e788931aff663def
Eduardo <EduardoGarcia360@users.noreply.github.com>
2024-10-17 09:29:57 -0600

    crud notas

--- public/header.php
+++ public/header.php
@@ -67,6 +67,7 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item" href="../../../../analisis2_notas/public/procesos/inscripciones/">Inscripciones</a>
                         <a class="collapse-item" href="../../../../analisis2_notas/public/procesos/asignaciones/">Asignaciones</a>
+                        <a class="collapse-item" href="../../../../analisis2_notas/public/procesos/notas/">Notas</a>
                     </div>
                 </div>
             </li>

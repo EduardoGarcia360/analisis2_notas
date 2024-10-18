7e90edf7c4e70bf789e7cd4b8b88cb83382186f0
Eduardo <EduardoGarcia360@users.noreply.github.com>
2024-10-18 09:21:40 -0600

    quitar logout

--- public/header.php
+++ public/header.php
@@ -99,15 +99,6 @@
                                 <img class="img-profile rounded-circle"
                                     src="../../../../analisis2_notas/assets/img/undraw_profile.svg">
                             </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
                         </li>
 
                     </ul>
